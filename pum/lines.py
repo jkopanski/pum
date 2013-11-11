227478ebf6564b1c17c9def3a6eb272b107a13db
Jakub Kopanski <jkopansk@gmail.com>
2013-11-11 17:46:13 +0100

    Rozwiazanie zadania 3

--- pum/lines.py
+++ pum/lines.py
@@ -1,9 +1,15 @@
-from scipy import constants
+import scipy.constants as const
 import numpy as np
 
 def coax_z(a, b, mu, epsilon): # impedance of coaxial line
-    return np.sqrt(constants.mu_0 * mu / ( constants.epsilon_0 * epsilon)) * np.log( a / b) / ( 2 * constants.pi)
+    return np.sqrt(const.mu_0 * mu / ( const.epsilon_0 * epsilon)) * np.log( a / b) / ( 2 * const.pi)
 
 def skew_coax_z( c, a, b, mu, epsilon):
     x = ( b + ( a * a - ( 4 * c * c)) / b) / (2 * a)
     return 59.952 * np.sqrt( mu / epsilon) * np.log( x + np.sqrt( x * x - 1))
+
+def cylindrical_flat( d, b, mu, epsilon): # impedance of cylindrical flat line
+    R = const.pi * d / (4 * b)
+    x = 1 + 2 * np.sinh( R) ** 2
+    y = 1 - 2 * np.sin( R) ** 2
+    return 59.952 * np.sqrt( mu / epsilon) * ( np.log( np.sqrt( x) + np.sqrt( y) / np.sqrt( x - y)) - R**4 / 30 + 0.014 * ( R**8))
---
+++ zad3.py
@@ -0,0 +1,22 @@
+import scipy.constants as const
+import scipy.optimize  as opt
+import numpy           as np
+import pum.algorithms  as alg
+from pum.lines import *
+
+b = 9 * const.milli
+
+def find_z( d, mu, eps):
+    return cylindrical_flat( d, b, mu, eps) - 30
+
+my_d = alg.newton_raphson( find_z, 0.01 * const.milli, 3.99 * const.milli, tol=1e-10, args = ( 1, 1))
+d = opt.newton( find_z, my_d, args = ( 1, 1));
+
+print 'py d = {} mm' .format( d / const.milli)
+print 'my d = {} mm' .format( my_d / const.milli)
+
+z0 = cylindrical_flat( my_d, b, 1, 1)
+z1 = cylindrical_flat( my_d, b, 2.04, 1)
+print 'Z(mu_r = 1; eps_r = 1) = {}' .format( z0)
+print 'Z(mu_r = 2.04; eps_r = 1) = {}' .format( z1)
+print 'Z change = {}' .format(z1 - z0)
